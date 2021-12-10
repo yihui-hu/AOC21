a2663e54b50babfc0134a00a1f63369ae200e00f
yihui <90987235+yihui-hu@users.noreply.github.com>
2021-12-10 02:13:22 -0500

    Update adventday10.cpp

--- day10/adventday10.cpp
+++ day10/adventday10.cpp
@@ -23,6 +23,7 @@ int main()
     deque<char> open;
     
     // crude key, would definitely use ro, co, so, ao instead of wasting time with underscores.. dummy
+    // i imagine you could use maps somehow, but i don't know much about it yet and i am quite tired
     char r_o = '(';
     char c_o = '{';
     char s_o = '[';
@@ -36,30 +37,35 @@ int main()
     vector<long> results;
     
     for (int i = 0; i < lines.size(); i++) {
-        string line = lines[i];
-        bool valid = true; // to check if closing bracket is valid, if not break out of loop immediately and return offender or do smth else
-        long sum = 0;
         
+        string line = lines[i];
+        bool valid = true; // to check if closing bracket is valid, if not break out of loop immediately and return invalid closing bracket or do smth else
+        long sum = 0; // useful for part 2 and 1
+        
+        // for checking through each character of the line
         for (int j = 0; j < line.length(); j++) {
+            
+            // if char is an opening bracket, add it to the deque
             if (line[j] == r_o or 
                 line[j] == c_o or 
                 line[j] == s_o or 
                 line[j] == a_o) {
                 open.push_front(line[j]);
-            } // if it is an opening bracket, add it to the deque
+            }
             
             char curr = open.front();
             cout << curr << endl; // debugging
             char next = line[j+1]; // check next character in the same iteration of loop
-
+            
+            // if it is a closing bracket, check if it corresponds with the opening bracket at front of deque
             if (next == r_c or 
                 next == c_c or 
                 next == s_c or 
-                next == a_c) { // if it is a closing bracket, check if it corresponds with the opening bracket at front of deque
+                next == a_c) { 
                     
                 if (curr == r_o and next != r_c) {
-                    errors.push_back(next); // for part 1 tracking
-                    valid = false; // if it does not match, return false so it breaks out of the loop. this line is donezo, move onto the next
+                    errors.push_back(next); // add to vector for part 1 purposes
+                    valid = false; // if it does not match, return false so it breaks out of the loop later on. this line is donezo, move onto the next
                 } else if (curr == c_o and next != c_c) {
                     errors.push_back(next);
                     valid = false;
@@ -74,7 +80,7 @@ int main()
             }
             if (not valid) {
                 open.clear(); // clear open for the next line
-                break;
+                break; // immediately move onto next line
             }
         }
         
@@ -105,27 +111,27 @@ int main()
         }
         cout << "END" << endl;
         
-        if (sum > 0) { // for lines that are complete or have an offending closing bracket, sum will be 0, so check for anything more than 0
+        if (sum > 0) { // for lines that are complete or have an invalid closing bracket, sum will be 0, so check for anything more than 0
             results.push_back(sum);
         }
         sum = 0;
     }
     
+    // sort array :^)
     sort(results.begin(), results.end());
 
     // spec says that there will always be an odd no. of scores to consider, so find median using simple formula:
     int median = (results.size()-1)/2;
-  
-//     for (long m : results) {
-//         cout << m << endl;
-//     }
-  
+    
     cout << results[median] << endl;
+  
+    // debugging stuff:
+    //     for (long m : results) {
+    //         cout << m << endl;
+    //     }
     
     // part 1 stuff
     
-    // int sum = 0;
-    // 
     // for (char m : errors) {
     //     if (m == c_c) {
     //         sum += 1197;
