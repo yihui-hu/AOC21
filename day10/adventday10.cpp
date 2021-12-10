cbe320d0e8e604af65d13b9cfaffe6ef8d42648b
yihui <90987235+yihui-hu@users.noreply.github.com>
2021-12-10 02:06:52 -0500

    Create adventday10.cpp

---
+++ day10/adventday10.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+#include <fstream>
+#include <string> 
+#include <vector>
+#include <bits/stdc++.h> 
+#include <sstream>
+#include <deque>
+
+using namespace std;
+
+int main()
+{
+    ifstream f("advent10.txt");
+    
+    string in;
+    vector<string> lines;
+    
+    while (getline(f, in)) {
+        lines.push_back(in);
+    }
+    
+    // had to teach myself deque for this shit... 2am vibes
+    deque<char> open;
+    
+    // crude key, would definitely use ro, co, so, ao instead of wasting time with underscores.. dummy
+    char r_o = '(';
+    char c_o = '{';
+    char s_o = '[';
+    char a_o = '<';
+    char r_c = ')';
+    char c_c = '}';
+    char s_c = ']';
+    char a_c = '>';
+    
+    vector<char> errors;
+    vector<long> results;
+    
+    for (int i = 0; i < lines.size(); i++) {
+        string line = lines[i];
+        bool valid = true; // to check if closing bracket is valid, if not break out of loop immediately and return offender or do smth else
+        long sum = 0;
+        
+        for (int j = 0; j < line.length(); j++) {
+            if (line[j] == r_o or 
+                line[j] == c_o or 
+                line[j] == s_o or 
+                line[j] == a_o) {
+                open.push_front(line[j]);
+            } // if it is an opening bracket, add it to the deque
+            
+            char curr = open.front();
+            cout << curr << endl; // debugging
+            char next = line[j+1]; // check next character in the same iteration of loop
+
+            if (next == r_c or 
+                next == c_c or 
+                next == s_c or 
+                next == a_c) { // if it is a closing bracket, check if it corresponds with the opening bracket at front of deque
+                    
+                if (curr == r_o and next != r_c) {
+                    errors.push_back(next); // for part 1 tracking
+                    valid = false; // if it does not match, return false so it breaks out of the loop. this line is donezo, move onto the next
+                } else if (curr == c_o and next != c_c) {
+                    errors.push_back(next);
+                    valid = false;
+                } else if (curr == s_o and next != s_c) {
+                    errors.push_back(next);
+                    valid = false;
+                } else if (curr == a_o and next != a_c) {
+                    errors.push_back(next);
+                    valid = false;
+                }
+                open.pop_front(); // if it matches, remove it from the deque
+            }
+            if (not valid) {
+                open.clear(); // clear open for the next line
+                break;
+            }
+        }
+        
+        // well, since we already have a deque of opening brackets, return the straddlers that don't have a match after running through the entire line
+      
+        cout << "RUN: " << i+1 << endl;
+        while (!open.empty() and valid) { // must be a valid line that ends abruptly
+            char yup = open.front();
+            cout << yup << endl; // useful for debugging
+            // add to a sum of sorts for each element in deque, then remove it and move on
+            if (yup == r_o) {
+                sum = sum * 5;
+                sum += 1;
+                open.pop_front();
+            } else if (yup == c_o) {
+                sum = sum * 5;
+                sum += 3;
+                open.pop_front();
+            } else if (yup == s_o) {
+                sum = sum * 5;
+                sum += 2;
+                open.pop_front();
+            } else if (yup == a_o) {
+                sum = sum * 5;
+                sum += 4;
+                open.pop_front();
+            }
+        }
+        cout << "END" << endl;
+        
+        if (sum > 0) { // for lines that are complete or have an offending closing bracket, sum will be 0, so check for anything more than 0
+            results.push_back(sum);
+        }
+        sum = 0;
+    }
+    
+    sort(results.begin(), results.end());
+
+    // spec says that there will always be an odd no. of scores to consider, so find median using simple formula:
+    int median = (results.size()-1)/2;
+  
+//     for (long m : results) {
+//         cout << m << endl;
+//     }
+  
+    cout << results[median] << endl;
+    
+    // part 1 stuff
+    
+    // int sum = 0;
+    // 
+    // for (char m : errors) {
+    //     if (m == c_c) {
+    //         sum += 1197;
+    //     } else if (m == s_c) {
+    //         sum += 57;
+    //     } else if (m == a_c) {
+    //         sum += 25137;
+    //     } else if (m == r_c) {
+    //         sum += 3;
+    //     }
+    // }
+    // 
+    // cout << sum << endl;
+    
+    return 0;
+}
+
+// char recursive() {
+//  // i wanted to do some recursive shit but gave up the ghost 
+// }
